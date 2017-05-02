f8b26b7fd04a4beb8cc83932348f74466367f605
castlchef <beaumont_jonny@hotmail.co.uk>
2017-05-02 17:44:45 +0100

    Added test runner for GameLogicTest and MapTest

---
+++ src/AllTests.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Created by jonny on 02/05/17.
+ */
+
+@RunWith(Suite.class)
+
+@SuiteClasses({MapTest.class, GameLogicTest.class})
+public class AllTests {}
